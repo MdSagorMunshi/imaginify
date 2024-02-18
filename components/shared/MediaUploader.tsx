210fda4ad45b42f1244a420a4395719567a9ca39
MdSagorMunshi <mi.sagor56@gmail.com>
2024-02-18 21:45:51 +0600

    initial commit

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="sagor_imaginify"
       options={{
         multiple: false,
         resourceType: "image",
